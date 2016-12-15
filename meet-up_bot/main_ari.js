ef5777971ee15d824f11db4acac7b39e3730fb11
ari-xb <rexb83@gmail.com>
2016-12-15 19:24:27 +1100

    Meetup API call working, but req URL hardcoded

--- meet-up_bot/main_ari.js
+++ meet-up_bot/main_ari.js
@@ -2,7 +2,9 @@
 
 var Botkit = require('botkit')
 var slackToken = process.env.SLACK_TOKEN
-var $ = require('jQuery'); //npm install jQuery
+var $ = require('jquery'); //npm install jQuery
+var axios = require('axios')
+
 // var Witbot = require('witbot')
 // var witToken = process.env.WIT_TOKEN
 // var witbot = Witbot(witToken)
@@ -40,52 +42,71 @@ controller.hears(['hello'],['direct_message','direct_mention','mention'],functio
 controller.hears(['busy'],['direct_message','direct_mention','mention'],function(bot,message) {
 
   // bot.reply(message,'typing...');
-  bot.reply({ type: "typing", channel: message.channel });
+  bot.replyWithTyping(message,'How can I be of service, now?');
 });
 
 
 controller.hears(['meetup'],['direct_message','direct_mention','mention'],function(bot,message) {
 
-  bot.reply(message,'getting that for you now...')
-
+  bot.replyWithTyping(message,'getting that for you now...')
 //
 // // make AJAX request to meetup.com
-  var settings = {
-    url: 'https://api.meetup.com',  // required
-    data: { zip: 11211,
-          radius: 1,
-          category: 25,
-          order: 'members',
-          sign: true }, // to go after the query string '?s='
-    method: '/find/groups',  // default optional
-    datatype: 'json'  // usually auto detected
-  }
+  var settings = 'https://api.meetup.com/find/events?photo-host=public&sig_id=213030423&fields=node.js&sig=7d8006176172cdd0d15c32eb983a08cdecf59fed';
+//  {
+    //https://api.meetup.com/find/groups?photo-host=public&page=5&text=ruby&sig_id=213030423&sig=57a89afda3eacca17d99c58187ff73b692871527
+// working searh for the text 'ruby'
+//    baseURL: 'https://api.meetup.com/',
+//    url: "/find/groups",  // required
+  //   data: {
+  //         zip: '11211',
+  //         radius: '1',
+  //         category: '25',
+  //         order: 'members',
+  //         sig_id: '213030423',
+  //         sig: '37c9d3f7211569dbf5599b620ba6eefb88794478'
+  //       }, // added Authed Signed URL.
+  //   method: 'get',  // default optional
+  //   dataType: 'json'  // usually auto detected
+  // }
 
-  console.log(settings);
+//var querystring = require('querystring');
+//axios.post('http://something.com/', querystring.stringify({ foo: 'bar' });
 
 // // //https://api.meetup.com/find/groups?zip=11211&radius=1&category=25&order=members&&sign=true
 // // // make an AJAX request to meet up api
     // var apiCall = function (settings) {
-    //   $.ajax(settings).done(function(res) {
+      axios.get(settings)
+      .then(function(res) {
     //
-    //     var meetups = res.results; //results is the
-    //     console.log(meetups);
-    //
-    //     // meetups.forEach(function(meetup) {
-    //     //
-    //     //   var $row = $('<h2>').append($('<a>') // create an a tag
-    //     //   .attr('target', '_blank')  // adding attributes to the a tag
-    //     //   .attr('href', 'http://www.imdb.com/title/' + movie.imdbID)  // forming the link
-    //     //   .text(movie.Title)); // create an a tag with text of movie title in it.
-    //     //
-    //     //   $('#list').append($row)
-    //     //
-    //     // })
-    //   });
+        // var meetups = res.results; //results is the
+        console.log(res);
+
+        // meetups.forEach(function(meetup) {
+        //
+        //   var $row = $('<h2>').append($('<a>') // create an a tag
+        //   .attr('target', '_blank')  // adding attributes to the a tag
+        //   .attr('href', 'http://www.imdb.com/title/' + movie.imdbID)  // forming the link
+        //   .text(movie.Title)); // create an a tag with text of movie title in it.
+        //
+        //   $('#list').append($row)
+        //
+        // })
+      })
+      .catch(function (error) {
+      console.log(error);
+      });
     // };
     // apiCall(settings);
 });
 
+// details to grab from meetup searches: maybe the first 5 indexes[0...4]
+// // [{
+//   "time":1481787000000, //time is in seconds from 1st Jan 1970 (Unix time)
+//   "group":{
+//   "name":"St Kilda 3182"
+//   },
+//   "link":"https://www.meetup.com/St-Kilda-3182/events/230764040/"
+//   }] Use the name with the MU page to create a clickable link, and date of the next MU.
 
 // Wit.ai integration code example:
 // controller.hears('.*', 'direct_message,direct_mention', function (bot, message) {
@@ -99,21 +120,5 @@ controller.hears(['meetup'],['direct_message','direct_mention','mention'],functi
 // controller.hears(['lunch', 'menu'],['direct_message','direct_mention','mention'],function(bot,message) {
 //
 //   bot.reply(message,'The menu for today is Mexican burritos.');
-//
-// });
-//
-// // user confirms
-// controller.hears(['yes', 'confirm'],['direct_message','direct_mention'],function(bot,message) {
-//
-//   bot.reply(message,'Mexican burritos on the way ;)');
-//   // TODO add user to the list
-//
-// });
-//
-// // user declines
-// controller.hears(['no'],['direct_message','direct_mention'],function(bot,message) {
-//
-//   bot.reply(message,'Perhaps you can join us tomorrow.');
-//   // TODO remove user from the list
 //
 // });
